b09939b45efbe316cc88c278b449c0606bf8b0d3
enriqueram03 <ramirezramos2003@gmail.com>
2021-04-13 09:42:28 -0700

    all of my work

---
+++ Basic HTML and CSS Projects/Basic_Javascript_1.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<script src="Hello_World.js"></script>
+</html>
+
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
